05feadf5bdf94b8a7a7c195866cd9eeeafbc0871
pfory <pfory@seznam.cz>
2018-06-14 13:33:27 +0200

    skript pro OTA update

---
+++ ota.sh
@@ -0,0 +1,5 @@
+﻿nahrát bin do /home/pi
+upravit verzi XXXX
+spustit v příkazové řádce
+
+python /home/$USER/.arduino15/packages/esp8266/hardware/esp8266/2.4.1/tools/espota.py -i 192.168.1.106 -p 8266 --auth=password -f /home/pi/AnemometerXXXX.ino.bin 
