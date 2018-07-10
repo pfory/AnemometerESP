4711e0765e2932c71b0f4b6f1a41ea20a9e31e85
pfory <pfory@seznam.cz>
2018-07-10 08:56:49 +0200

    kosmeticke upravy kodu

--- Anemometer.ino
+++ Anemometer.ino
@@ -246,15 +246,15 @@ void MQTT_connect() {
 
   uint8_t retries = 3;
   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       DEBUG_PRINTLN(mqtt.connectErrorString(ret));
-       DEBUG_PRINTLN("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-       retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         while (1);
-       }
+    DEBUG_PRINTLN(mqtt.connectErrorString(ret));
+    DEBUG_PRINTLN("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0) {
+      // basically die and wait for WDT to reset me
+      while (1);
+    }
   }
   DEBUG_PRINTLN("MQTT Connected!");
 }
