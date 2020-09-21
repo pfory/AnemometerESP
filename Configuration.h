c0615ae3e4641175b4628301c7d12fc5df540ab1
pfory <pfory@seznam.cz>
2020-09-21 15:16:52 +0200

    + double reset detector + timeouty na wifimanager

--- Configuration.h
+++ Configuration.h
@@ -1,8 +1,19 @@
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
 
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include <Ticker.h>
+#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
+#include <DoubleResetDetector.h>      //https://github.com/khoih-prog/ESP_DoubleResetDetector
+#include "Sender.h"
+#include <Wire.h>
+#include <Ticker.h>
+#include <timer.h>
+
+
 //SW name & version
-#define     VERSION                      "0.43"
+#define     VERSION                      "0.50"
 #define     SW_NAME                      "Anemometer"
 
 //#define timers
@@ -18,6 +29,17 @@
 #define HOSTNAMEOTA   "anemometer"
 #endif
 
+#ifdef serverHTTP
+#include <ESP8266WebServer.h>
+#endif
+
+#ifdef time
+#include <TimeLib.h>
+#include <Timezone.h>
+#endif
+
+#define CFGFILE "/config.json"
+
 
 /*
 --------------------------------------------------------------------------------------------------------------------------
@@ -28,14 +50,6 @@ Version history:
 HW
 ESP8266 Wemos D1
 */
-
-#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
-#include <Ticker.h>
-#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
-#include "DoubleResetDetector.h" // https://github.com/datacute/DoubleResetDetector
-#include "Sender.h"
-#include <Wire.h>
 
 #define verbose
 #ifdef verbose
@@ -52,6 +66,15 @@ ESP8266 Wemos D1
   #define DEBUG_PRINTLN(x)
   #define DEBUG_PRINTF(x, y)
 #endif 
+
+// Number of seconds after reset during which a
+// subseqent reset will be considered a double reset.
+#define DRD_TIMEOUT 2
+// RTC Memory Address for the DoubleResetDetector to use
+#define DRD_ADDRESS 0
+
+#define CONFIG_PORTAL_TIMEOUT 60 //jak dlouho zustane v rezimu AP nez se cip resetuje
+#define CONNECT_TIMEOUT 120 //jak dlouho se ceka na spojeni nez se aktivuje config portal
 
 static const char* const      mqtt_server                    = "192.168.1.56";
 static const uint16_t         mqtt_port                      = 1883;
@@ -76,7 +99,4 @@ static const char* const      mqtt_topic_restart             = "restart";
 #define SEND_DELAY                          5000  //prodleva mezi poslanim dat v ms
 #define SENDSTAT_DELAY                      60000 //poslani statistiky kazdou minutu
 
-#define CFGFILE "/config.json"
-
-                                     
 #endif
